fba2aa56689b702010e2d87dffdb66e25ffd522f
shiraz19-meet <shiraz19@meet.mit.edu>
2017-07-27 14:18:08 +0300

    Stopping here

---
+++ fun1.py
@@ -0,0 +1,13 @@
+# this block of code is the sums all numbers from 0-100
+
+#def add_numbers():
+   # c=0
+   # for number in range(1,100+1):
+  #      print(number)
+ #       c=c+number
+ #   return (c)
+
+#answer= add_numbers()
+#print(answer)
+
+def add_numbers
